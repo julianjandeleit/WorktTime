b446bd338d834b6a1b9c1352603a54bae8dbbdba
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2022-09-13 19:21:25 +0200

    add summary

--- worktime.py
+++ worktime.py
@@ -5,6 +5,7 @@ from codecs import getdecoder
 from os import times
 from time import time
 import arrow
+import datetime
 import yaml
 
 from enum import Enum
@@ -13,6 +14,8 @@ from dataclasses import dataclass
 import click
 import pathlib
 import pyTextColor as ptc
+
+from functools import reduce
 
 # don't print yaml tags
 yaml.emitter.Emitter.process_tag = lambda self, *args, **kw: None
@@ -76,6 +79,11 @@ def insert_record(path: pathlib.Path, rec: WorkRecord) -> None:
         raise ValueError("Found Unexpected RecordType make sure that you have started/closed the last session!")
     records.append(rec)
     write_records(path,records)
+    
+def seconds_to_hours(seconds: float) -> float:
+    minutes = seconds / 60.0
+    hours = minutes / 60.0
+    return hours
 
 # %%
 
@@ -121,6 +129,30 @@ def status():
     timediff = last_rec.timestamp.humanize(granularity=["hour", "minute"])
     print(f"Current Status\n{last_rec.type.name} - {timediff}")
     
+@cli.command()
+def summary():
+    recs = read_records(WORKTIME_PATH) 
+    recs = sorted(recs,key=lambda x: x.timestamp)
+    
+    # simulate current stop if already in session
+    if recs[-1].type == RecordType.START:
+        recs.append(make_record(RecordType.END))
+        
+    durations = []
+    for startRec, endRec in zip(recs[0::2], recs[1::2]):
+        sess_dur = endRec.timestamp - startRec.timestamp
+        print(f"Session {endRec.timestamp.humanize()}:\t{round(seconds_to_hours(sess_dur.total_seconds()),1)} h")
+        durations.append(sess_dur)
+        
+    summed :datetime.timedelta = reduce(lambda x,y: x+y,durations)
+    total_seconds = summed.total_seconds()
+    
+    minutes = total_seconds / 60.0
+    hours = minutes / 60.0
+    
+    print(f"\nTotal WorkTime:\t\t{hours} h")
+    
 
 if __name__ == "__main__":
     cli()
+# %%
